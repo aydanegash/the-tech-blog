89e61f03ec2cca4848f48788b2774ed00f3a8091
aydanegash <73369360+aydanegash@users.noreply.github.com>
2021-08-05 21:23:35 -0500

    worked on views and making it into handlebars

---
+++ controllers/api/postRoutes.js
@@ -0,0 +1,7 @@
+const router = require('express').Router();
+const { Post } = require('../../models/Post');
+//const withAuth = require('../../utils/auth');
+
+
+
+module.exports = router;
--- server.js
+++ server.js
@@ -2,10 +2,33 @@
 const express = require('express');
 const path = require('path');
 const session = require('express-session');
-
+const exphbs = require('express-handlebars');
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3002;
+
+// const Sequelize = require('sequelize');
+// require('dotenv').config();
+
+// let sequelize;
+
+// if (process.env.JAWSDB_URL) {
+//   sequelize = new Sequelize(process.env.JAWSDB_URL);
+// } else {
+//   sequelize = new Sequelize(
+//     process.env.DB_NAME,
+//     process.env.DB_USER,
+//     process.env.DB_PASSWORD,
+//     {
+//       host: 'localhost',
+//       dialect: 'mysql',
+//       port: 3306
+//     }
+//   );
+// }
+
+//module.exports = sequelize;
+
 
 
 app.listen(PORT, () => {
